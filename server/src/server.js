0ab3f2d28810fdc2b170fc95d9a5266aab5519d9
Aditya Rawat <adityarawatnew2487@gmail.com>
2026-05-12 14:49:38 +0530

    starting develpement first fase

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,17 @@
 import express from "express"
 import dotEnv from 'dotenv'
-
+import cors from 'cors'
 dotEnv.config({
     debug:true
 })
 
 const app = express()
+app.use(cors({
+    allowedHeaders:["Authorization","x-cdn-base-auth"],
+    credentials:true,
+    origin:["http://localhost:3000"] ,
+    methods:["OPTIONS", "POST","GET", "PUT", "DELETE","PATCH"]
+}))
 const PORT = process.env.PORT || 8000
 app.get("/",(req,res)=>{
     return res.json({
